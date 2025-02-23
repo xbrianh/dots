0b79ea138c99b1d80aeebc4ce017c3248c5a9267
Sara Leslie <saramleslie@gmail.com>
2025-02-22 17:18:58 -0800

    wip

--- gen_dots.py
+++ gen_dots.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 import os
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
 import dots
 
 
-NUMBER_OF_FILES = 11
-OUTPUT_FILE_TEMPLATE = "foo/{image_number:03}.png"
-DOT_PARMS = {
-    "number_of_dots": 40,
+NUMBER_OF_PAIRS = 11
+OUTPUT_FILE_TEMPLATE = "foo/dots_{image_number}.png"
+
+DOT_PARMS_A = {
+    "number_of_dots": 10,
+    "total_dot_area": 24000,
+    "desired_hull" : 140000.0,
+    "number_of_tries": 10,
+    "shape": "circle",
+}
+
+DOT_PARMS_B = {
+    "number_of_dots": 45,
     "total_dot_area": 24000,
     "desired_hull" : 140000.0,
     "number_of_tries": 10,
@@ -16,17 +25,25 @@ DOT_PARMS = {
 }
 
 
-def generate_and_save_dots(image_number: int):
-    coords, radii = dots.generate_dots(**DOT_PARMS)
+def generate_and_save_dots(image_number: int, **dot_parms):
+    coords, radii = dots.generate_dots(**dot_parms)
     image = dots.draw_dots(coords, radii)
     image.save(OUTPUT_FILE_TEMPLATE.format(image_number=image_number))
 
 
-def gen_dots(number_of_files: int):
+def gen_dots(number_of_pairs: int):
+    image_number = 1
+    futures = list()
     with ProcessPoolExecutor() as e:
-        e.map(generate_and_save_dots, [image_number for image_number in range(number_of_files)])
+        for _ in range(number_of_pairs):
+            futures.append(e.submit(generate_and_save_dots, image_number, **DOT_PARMS_A))
+            image_number += 1
+            futures.append(e.submit(generate_and_save_dots, image_number, **DOT_PARMS_B))
+            image_number += 1
+            for f in as_completed(futures):
+                f.result()
 
 
 if __name__ == "__main__":
     os.makedirs(os.path.dirname(OUTPUT_FILE_TEMPLATE), exist_ok=True)
-    gen_dots(NUMBER_OF_FILES)
+    gen_dots(NUMBER_OF_PAIRS)
